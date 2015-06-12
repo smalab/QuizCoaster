57ca31d053931141ae11d063eb211ade6bfe18ca
oitsmalab <oitsmalab@gmail.com>
2015-06-12 16:09:26 +0900

    ジャイロで選択したら色が変わるようにする途中です

---
+++ QC2/Assets/Count.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Count : MonoBehaviour {
+	//private DateTime _startTime;
+	//private readonly int _limitTime = 10;
+	/*public GameObject number;*/
+	//private float time = 3.0f;
+	public Image count;
+
+public IEnumerator CountdownCoroutine(){
+		count.gameObject.SetActive(true);
+		
+		count.color = new Color(0, 0, 0);
+		yield return new WaitForSeconds(1.0f);
+		
+
+		count.color =  new Color(255, 0, 0);
+		yield return new WaitForSeconds(1.0f);
+		
+
+		count.color =  new Color(0, 255, 0);
+		yield return new WaitForSeconds(1.0f);
+		
+
+		count.color =  new Color(0, 0, 255);
+		yield return new WaitForSeconds(1.0f);
+		
+		count.gameObject.SetActive(false);
+	}
+}
--- QC2/Assets/CountDownGyro.cs
+++ QC2/Assets/CountDownGyro.cs
@@ -2,38 +2,45 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class jayro : MonoBehaviour {
-
+public class CountDownGyro : MonoBehaviour {
+	
 	public Image image1;
 	public Image image2;
 	private string gyro;
-
+	
 	// Use this for initialization
 	void Start () {
 		Input.gyro.enabled = true;
 	}
 
-	// Update is called once per frame
 	void Update () {
 		//Debug.Log ("x軸の加速度: " + Input.acceleration.x);
 		//Debug.Log ("y軸の加速度: " + Input.acceleration.y);
 		//Debug.Log ("z軸の加速度: " + Input.acceleration.z);
-
+		Count countdown = GetComponent<Count>();
+		
 		if (/*Input.acceleration.x > 0 && */Input.gyro.rotationRateUnbiased.y < -3.0f) {
-
+			
 			transform.position += new Vector3(0.01f, 0, 0);
 			image1.color = new Color (0, 0, 0);
 			image2.color = new Color (255, 255, 255);
 			Debug.Log("y" + Input.gyro.rotationRateUnbiased.y);
+
+			countdown.StartCoroutine("CountdownCoroutine");
+
 		} /*else*/ if (/*Input.acceleration.x > 0 && */Input.gyro.rotationRateUnbiased.y > 3.0f) {
 			transform.position += new Vector3(-0.01f, 0, 0);
 			image2.color = new Color (0, 0, 0);
 			image1.color = new Color (255, 255, 255);
 			Debug.Log ("y" + Input.gyro.rotationRateUnbiased.y);
+
+			countdown.StartCoroutine("CountdownCoroutine");
+
+
 		} /*else {
 			image1.color = new Color (255, 255, 255);
 			image2.color = new Color (255, 255, 255);
 		}*/
 	}
-
+	
 }
