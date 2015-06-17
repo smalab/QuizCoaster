bcbefe5c9339c88079ea9dc946725fc9203ab5d4
oitsmalab <oitsmalab@gmail.com>
2015-06-17 16:10:16 +0900

    #1のは完成した。 #2はGitHub/QuizCoaster/qustionselect6_17にあり、オブジェクトに画像を貼り付けるのをスクリプトから出来た。

---
+++ qustionselect6_17/Assets/TextureManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TextureManager : MonoBehaviour
+{
+
+	const string BASE_TEXTURE = "Textures/apple";
+	Texture2D image;
+	public GameObject go;
+
+
+	void Start() {
+		image = Resources.Load(BASE_TEXTURE) as Texture2D;
+		//image = Resources.Load<Texture2D>(BASE_TEXTURE);
+		go.GetComponent<Renderer>().GetComponent<Renderer>().material.mainTexture = image;
+	}
+
+	void Update() {
+
+	}
+}
