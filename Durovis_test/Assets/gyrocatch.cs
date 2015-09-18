84d2a68749e55858d835adfdf7770c99fbfe9925
smalab <oitsmalab@gmail.com>
2015-09-18 11:34:07 +0900

    gyrocatchのスクリプトのlogSaveでcsvファイルを作成し、書き込むことができた。

---
+++ Durovis_test/Assets/gyrocatch.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+using System.Text;
+
+public class gyrocatch : MonoBehaviour {
+
+
+	private int x = 0;
+	private int y = 0;
+	private int z = 0;
+	private string x1;
+	private string y1;
+	private string z1;
+
+
+	// Use this for initialization
+	void Start () {
+		Input.gyro.enabled = true;
+		if (File.Exists("Asset/test4.csv")) {
+			FileStream f = new FileStream("test.txt", FileMode.Open, FileAccess.Read);
+			f.SetLength (0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		x = Mathf.RoundToInt(Input.gyro.attitude.x * 10.0f) + 5;
+		y = Mathf.RoundToInt(Input.gyro.attitude.y * 10.0f) - 5;
+		z = Mathf.RoundToInt(Input.gyro.attitude.z * 10.0f) - 5;
+		x1 = x.ToString();
+		y1 = y.ToString();
+		z1 = z.ToString();
+		//Debug.Log (x + " " + y + " " + z);
+		logSave(x1, y1, z1);
+
+	}
+
+	public void logSave(string x, string y, string z){
+
+		FileStream f = new FileStream("Assets/test4.csv", FileMode.Append, FileAccess.Write);
+		Encoding utf8Enc = Encoding.GetEncoding("UTF-8");
+		StreamWriter writer = new StreamWriter(f, utf8Enc);
+		writer.Write(x + ",");
+		writer.Write(y + ",");
+		writer.WriteLine(z);
+		writer.Close();
+
+	}
+}
