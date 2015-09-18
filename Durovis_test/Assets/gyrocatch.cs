a501b0f15ac08e98616f788dde2b8e6707d21b00
smalab <oitsmalab@gmail.com>
2015-09-18 12:34:52 +0900

    アプリを起動させる度に1行さげてcsvファイルにセンサーの値をとる様にした。0.2秒毎に1回値をとる。

--- Durovis_test/Assets/gyrocatch.cs
+++ Durovis_test/Assets/gyrocatch.cs
@@ -13,38 +13,47 @@ public class gyrocatch : MonoBehaviour {
 	private string x1;
 	private string y1;
 	private string z1;
+	private float time = 0.0f;
 
 
 	// Use this for initialization
 	void Start () {
 		Input.gyro.enabled = true;
-		if (File.Exists("Asset/test4.csv")) {
-			FileStream f = new FileStream("test.txt", FileMode.Open, FileAccess.Read);
-			f.SetLength (0);
+		if (File.Exists("Assets/log.csv")) {
+			Debug.Log("fileatta");
+			FileStream f = new System.IO.FileStream("Assets/log.csv", FileMode.Append, FileAccess.Write);
+			Encoding utf8Enc = Encoding.GetEncoding("UTF-8");
+			StreamWriter writer = new StreamWriter(f, utf8Enc);
+			writer.WriteLine("");
+			writer.Close();
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		x = Mathf.RoundToInt(Input.gyro.attitude.x * 10.0f) + 5;
-		y = Mathf.RoundToInt(Input.gyro.attitude.y * 10.0f) - 5;
-		z = Mathf.RoundToInt(Input.gyro.attitude.z * 10.0f) - 5;
-		x1 = x.ToString();
-		y1 = y.ToString();
-		z1 = z.ToString();
-		//Debug.Log (x + " " + y + " " + z);
-		logSave(x1, y1, z1);
+		time += Time.deltaTime;
+		if (time >= 0.2f) {
+			x = Mathf.RoundToInt (Input.gyro.attitude.x * 10.0f) + 5;
+			y = Mathf.RoundToInt (Input.gyro.attitude.y * 10.0f) - 5;
+			z = Mathf.RoundToInt (Input.gyro.attitude.z * 10.0f) - 5;
+			x1 = x.ToString ();
+			y1 = y.ToString ();
+			z1 = z.ToString ();
+			Debug.Log (x + " " + y + " " + z);
+			logSave (x1, y1, z1);
+			time = 0.0f;
+		}
 
 	}
 
 	public void logSave(string x, string y, string z){
 
-		FileStream f = new FileStream("Assets/test4.csv", FileMode.Append, FileAccess.Write);
+		FileStream f = new FileStream("Assets/log.csv", FileMode.Append, FileAccess.Write);
 		Encoding utf8Enc = Encoding.GetEncoding("UTF-8");
 		StreamWriter writer = new StreamWriter(f, utf8Enc);
 		writer.Write(x + ",");
 		writer.Write(y + ",");
-		writer.WriteLine(z);
+		writer.Write(z + ",");
 		writer.Close();
 
 	}
