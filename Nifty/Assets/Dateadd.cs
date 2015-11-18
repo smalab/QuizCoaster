efb43f9da76a9c9c9d6b6cb44d6c0eac67bc0827
smalab <oitsmalab@gmail.com>
2015-11-18 11:48:14 +0900

    GitHubファイル内にNiftyのプロジェクトを移動。

---
+++ Nifty/Assets/Dateadd.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using NCMB;
+using System.Collections.Generic;
+
+public class Dateadd : MonoBehaviour {
+
+	float time = 0;
+	int number;
+	int a = 0;
+	bool flag = false;
+	string name = "a";
+	NCMBObject obj = new NCMBObject ("Book");
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("time : " + time);
+		Debug.Log ("count : " + a);
+		time = time + Time.deltaTime;
+		name += ",a,a,a,a,a,a,a,a";
+		/*if ((int)time % 2 == 1 && flag == true) {
+			obj.Add ("test", name);
+			obj.SaveAsync ((NCMBException e) => {      
+				if (e != null) {
+					Debug.Log ("erro:::::");//エラー処理
+				} else {
+					Debug.Log ("go:::::::");
+					//成功時の処理
+				}                   
+			});
+			a++;
+			flag = false;
+			time = 0.0f;
+		} else {
+			flag = true;
+		}*/
+		if (time > 60.0f) {
+			name += "bbbbb";
+			obj.Add ("test", name);
+			obj.SaveAsync ((NCMBException e) => {      
+				if (e != null) {
+					Debug.Log ("erro:::::");//エラー処理
+				} else {
+					Debug.Log ("go:::::::");
+					//成功時の処理
+				}                   
+			});
+		}
+	}
+}
---
+++ Nifty/Assets/Datepull.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using NCMB;
+using System.Collections.Generic;
+using System.Threading;
+
+public class Datepull : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		NCMBObject obj = new NCMBObject ("Book");
+		obj.Add ("Unity1", "fetchTest");
+		obj.SaveAsync ();
+		Thread.Sleep (3000);//非同期待ち
+		
+		//保存したオブジェクトのobjectIdをもとに取得を行う
+		NCMBObject obj2 = new NCMBObject ("Book");
+		obj2.ObjectId = obj.ObjectId;
+		obj2.FetchAsync ((NCMBException e) => {        
+			if (e != null) {
+				//エラー処理
+			} else {    
+				//成功時の処理
+			}               
+		});
+		Debug.Log ("a");
+		obj2.Increment ("test");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
