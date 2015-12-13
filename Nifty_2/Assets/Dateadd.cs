71eee0b56021d7f851daa78e7d3ddf58341370cd
sumarabo <oitsmalab@gmail.com>
2015-12-13 21:42:48 +0900

    Niftyのデータストアに記録しているObjectIDを入力することで、そのObjectIDのキーの値を取得し、csvファイルとして保存できた。

---
+++ Nifty_2/Assets/Dateadd.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+/*using NCMB;
+using System.Collections.Generic;*/
+
+public class Dateadd : MonoBehaviour {
+
+	public Camera camera;
+	public string date;
+	private string rotate_x;
+	private string rotate_y;
+	private string rotate_z;
+	DateTime dtNow = DateTime.Now;
+	
+
+	public float time = 0;
+	int number;
+	int a = 0;
+	bool flag = false;
+	string name = "a";
+	//NCMBObject obj = new NCMBObject ("Book");
+
+	// Use this for initialization
+	void Start () {
+		date += dtNow + ",";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time = time + Time.deltaTime;
+		if (Time.frameCount % 15 == 0) {
+			//Debug.Log(date);
+			rotate_x = Omit(camera.transform.rotation.x).ToString ();
+			rotate_y = Omit(camera.transform.rotation.y).ToString ();
+			rotate_z = Omit(camera.transform.rotation.z).ToString ();
+			date += rotate_x + "," + rotate_y + "," + rotate_z + ",";
+		}
+	}
+
+	public float Omit(float value){
+		value *= 100;
+		value = Mathf.Floor (value);
+		value /= 100;
+		return value;
+	}
+
+}
---
+++ Nifty_2/Assets/push.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using NCMB;
+using System.Collections.Generic;
+
+public class push : MonoBehaviour {
+
+	NCMBObject obj = new NCMBObject ("Book");
+	public GameObject a;
+	public static Dateadd dateadd;
+	// Use this for initialization
+	void Start () {
+		dateadd = a.GetComponent<Dateadd> ();
+		Debug.Log (dateadd.time);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void datepush(){
+		Debug.Log (dateadd.time);
+		obj.Add ("test", dateadd.date);
+		Debug.Log ("start");
+		obj.SaveAsync ((NCMBException e) => {      
+			if (e != null) {
+				Debug.Log ("erro:::::");//エラー処理
+			} else {
+				Debug.Log ("go:::::::");
+				//成功時の処理
+			}                   
+		});
+		Debug.Log ("OK");
+	}
+}
